633b1d0dde051d8fc1ca3ac2c3b74f89328de164
Lucas Barragana <lucasbarraga@gmail.com>
2024-03-29 12:30:28 -0300

    including project lp

--- src/app/data/db.js
+++ src/app/data/db.js
@@ -1,17 +1,17 @@
 const projects = [
     { 
-        id: 1, 
-        img: '/lp-8.jpg', 
-        title: 'Landing Page Desginer', 
-        description: 'Page of a professional graphic designer, aiming to increase her engagement and professionalism',
+        id: 6, 
+        img: '/tp-lp-1.png', 
+        title: 'Landing Page Trafego Digital', 
+        description: 'Page for a company who is specialized on paid digital traffic ',
         website:'Building !',
-        technologies: ['/angular.png', '/tailwind.png'], 
-        url: 'https://www.google.com.br', 
-        giturl: 'https://www.google.com.br',
+        technologies: ['/angular.png', '/Sass.png', '/ts.png'], 
+        url: 'https://lp-trafego-pago.vercel.app/', 
+        giturl: 'https://github.com/LucasBarragana/lp-trafego-pago',
         images: [''] 
     },
     { 
-        id: 2, 
+        id: 5, 
         img: '/BIG3.png', 
         title: 'BIG 3', 
         description: 'Are anime for kids? The more time passes, the more I realize how much cartoons have to teach us', 
@@ -22,7 +22,7 @@ const projects = [
         images: ['/BIG3.png', '/anime1.png','/anime2.png', '/anime3.png'] 
     },
     { 
-        id: 3, 
+        id: 2, 
         img: '/meu-portf.png', 
         title: 'Portifólio', 
         description: 'How you describe yourself says a lot about you. Nothing better to demonstrate that with a practical project', 
@@ -33,7 +33,7 @@ const projects = [
         images: ['/meu-portf.png', '/port-about.png','/port-skills.png', '/port-projects.png', '/port-contact.png'] 
     },
     { 
-        id: 4, 
+        id: 1, 
         img: '/lp-3.jpg', 
         title: 'Ecommerce Shop', 
         description: 'Project in development. This will be a complete e-commerce project', 
@@ -44,18 +44,18 @@ const projects = [
         images: [''] 
     },
     { 
-        id: 5, 
+        id: 4, 
         img: '/memories-login.png', 
         title: 'Memories', 
         description: 'Family website. Developed to keep memories. Distance can keep people apart, this website can bring them closer', 
         website:'No deploy',
         technologies: ['/react2.png','/redux.png','/css.png', '/mongo.png', ], 
-        url: '', 
+        url: 'https://github.com/LucasBarragana/Memorias', 
         giturl: 'https://github.com/LucasBarragana/Memorias',
         images: ['/memories-login.png', '/memories-posts.png','/memories-editposts.png', '/memories-editing.png','/memories=profile.png'] 
     },
     { 
-        id: 6, 
+        id: 3, 
         img: '/ST-BURGUER.png', 
         title: 'ST BURGUER', 
         description: 'Food ordering website, where the administrator can visually configure, add, or delete all menu items. And much more.',
