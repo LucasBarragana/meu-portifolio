319375247f593ff63e1d1bfa87cf62b2e27f394a
Lucas Barragana <lucasbarraga@gmail.com>
2024-03-29 17:32:26 -0300

    new photos commit

--- src/app/data/db.js
+++ src/app/data/db.js
@@ -1,17 +1,17 @@
 const projects = [
     { 
-        id: 6, 
-        img: '/tp-lp-1.png', 
-        title: 'Landing Page Trafego Digital', 
-        description: 'Page for a company who is specialized on paid digital traffic ',
+        id: 1, 
+        img: '/lp-3.jpg', 
+        title: 'Ecommerce Shop', 
+        description: 'Project in development. This will be a complete e-commerce project', 
         website:'Building !',
-        technologies: ['/angular.png', '/Sass.png', '/ts.png'], 
-        url: 'https://lp-trafego-pago.vercel.app/', 
-        giturl: 'https://github.com/LucasBarragana/lp-trafego-pago',
+        technologies: ['/angular.png','/tailwind.png','/ts.png'], 
+        url: 'https://www.google.com.br', 
+        giturl: 'https://www.google.com.br',
         images: [''] 
     },
     { 
-        id: 5, 
+        id: 2, 
         img: '/BIG3.png', 
         title: 'BIG 3', 
         description: 'Are anime for kids? The more time passes, the more I realize how much cartoons have to teach us', 
@@ -22,26 +22,15 @@ const projects = [
         images: ['/BIG3.png', '/anime1.png','/anime2.png', '/anime3.png'] 
     },
     { 
-        id: 2, 
-        img: '/meu-portf.png', 
-        title: 'Portifólio', 
-        description: 'How you describe yourself says a lot about you. Nothing better to demonstrate that with a practical project', 
+        id: 3, 
+        img: '/ST-BURGUER.png', 
+        title: 'ST BURGUER', 
+        description: 'Food ordering website, where the administrator can visually configure, add, or delete all menu items. And much more.',
         website:'Website',
-        technologies: ['/next.png','/tailwind.png'], 
-        url: 'https://meu-portifolio-beta-two.vercel.app/', 
-        giturl: 'https://github.com/LucasBarragana/meu-portifolio',
-        images: ['/meu-portf.png', '/port-about.png','/port-skills.png', '/port-projects.png', '/port-contact.png'] 
-    },
-    { 
-        id: 1, 
-        img: '/lp-3.jpg', 
-        title: 'Ecommerce Shop', 
-        description: 'Project in development. This will be a complete e-commerce project', 
-        website:'Building !',
-        technologies: ['/angular.png','/tailwind.png','/ts.png'], 
-        url: 'https://www.google.com.br', 
-        giturl: 'https://www.google.com.br',
-        images: [''] 
+        technologies: ['/next.png','/mongo.png', '/aws1.png','/tailwind.png'], 
+        url: 'https://st-burguer.vercel.app/', 
+        giturl: 'https://github.com/LucasBarragana/ST-BURGUER',
+        images: ['/ST-BURGUER.png', '/STFome-about.png','/STFome-menu.png', '/STFome-extras.png','/STFome-items.png','/STFome-profile.png', '/STFome-orders.png','/STFome-smenu.png'] 
     },
     { 
         id: 4, 
@@ -55,16 +44,29 @@ const projects = [
         images: ['/memories-login.png', '/memories-posts.png','/memories-editposts.png', '/memories-editing.png','/memories=profile.png'] 
     },
     { 
-        id: 3, 
-        img: '/ST-BURGUER.png', 
-        title: 'ST BURGUER', 
-        description: 'Food ordering website, where the administrator can visually configure, add, or delete all menu items. And much more.',
+        id: 5, 
+        img: '/meu-portf.png', 
+        title: 'Portifólio', 
+        description: 'How you describe yourself says a lot about you. Nothing better to demonstrate that with a practical project', 
         website:'Website',
-        technologies: ['/next.png','/mongo.png', '/aws1.png','/tailwind.png'], 
-        url: 'https://st-burguer.vercel.app/', 
-        giturl: 'https://github.com/LucasBarragana/ST-BURGUER',
-        images: ['/ST-BURGUER.png', '/STFome-about.png','/STFome-menu.png', '/STFome-extras.png','/STFome-items.png','/STFome-profile.png', '/STFome-orders.png','/STFome-smenu.png'] 
-    }
+        technologies: ['/next.png','/tailwind.png'], 
+        url: 'https://meu-portifolio-beta-two.vercel.app/', 
+        giturl: 'https://github.com/LucasBarragana/meu-portifolio',
+        images: ['/meu-portf.png', '/port-about.png','/port-skills.png', '/port-projects.png', '/port-contact.png'] 
+    },    
+    { 
+        id: 6, 
+        img: '/tp-lp-1.png', 
+        title: 'Landing Page Trafego Digital', 
+        description: 'A page for a company specialized in paid digital traffic that aims to increase the visibility of other companies.',
+        website:'Website',
+        technologies: ['/angular.png', '/Sass.png', '/ts.png'], 
+        url: 'https://lp-trafego-pago.vercel.app/', 
+        giturl: 'https://github.com/LucasBarragana/lp-trafego-pago',
+        images: ['/tp-lp-1.png', '/tp-lp-2.png', '/tp-lp-3.png', '/tp-lp-4.png'] 
+    },   
+    
+    
 ];
 
 export default projects
